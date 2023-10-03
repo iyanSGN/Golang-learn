8f03502b31cb71a383aa145bd904187efa6d4522
Iyan Siagian <iyansiagian04@gmail.com>
2023-10-03 16:07:22 +0700

    fix

--- app/kecamatan/dto.go
+++ app/kecamatan/dto.go
@@ -3,15 +3,17 @@ package kecamatan
 import "time"
 
 type KecamatanRequestDTO struct {
-	ID 			uint		`json:"id"`
-	IDKabKota 	*uint		`json:"id_kab_kota" validate:"required"`
-	Nama		string		`json:"nama" validate:"required"`
+	ID        uint   `json:"id"`
+	IDKabKota *uint  `json:"id_kab_kota" validate:"required"`
+	Nama      string `json:"nama" validate:"required"`
 }
 
 type KecamatanResponseDTO struct {
-	ID 				uint			`json:"id"`
-	Nama			string			`json:"nama"`
-	CreatedAt		time.Time		`json:"created_at"`
-	KabKota_id		*uint			`json:"kab_kota"`
-	KabKota_nama	string			`json:"kabupaten_nama"`
+	ID           uint      `json:"id"`
+	Nama         string    `json:"nama"`
+	CreatedAt    time.Time `json:"created_at"`
+	KabKota_id   *uint     `json:"kab_kota"`
+	KabKota_nama string    `json:"kabupaten_nama"`
+	ProvinceID   uint      `json:"province_id"`
+	ProvinceName string    `json:"province_name"`
 }
